96e8587e8556a92fb977b59527d7bb5b1732b99c
he1enaluik <helenakrislin.luik@tptlive.ee>
2025-12-11 18:18:17 +0200

    adsdasd

--- mm23blog/app/Http/Controllers/Auth/EmailVerificationNotificationController.php
+++ mm23blog/app/Http/Controllers/Auth/EmailVerificationNotificationController.php
@@ -5,20 +5,36 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+use Illuminate\View\View;
 
-class EmailVerificationNotificationController extends Controller
+class ConfirmablePasswordController extends Controller
 {
     /**
-     * Send a new email verification notification.
+     * Show the confirm password view.
+     */
+    public function show(): View
+    {
+        return view('auth.confirm-password');
+    }
+
+    /**
+     * Confirm the user's password.
      */
     public function store(Request $request): RedirectResponse
     {
-        if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended(route('dashboard', absolute: false));
+        if (! Auth::guard('web')->validate([
+            'email' => $request->user()->email,
+            'password' => $request->password,
+        ])) {
+            throw ValidationException::withMessages([
+                'password' => __('auth.password'),
+            ]);
         }
 
-        $request->user()->sendEmailVerificationNotification();
+        $request->session()->put('auth.password_confirmed_at', time());
 
-        return back()->with('status', 'verification-link-sent');
+        return redirect()->intended(route('dashboard', absolute: false));
     }
 }
